2fb938ba65921a688660ded96e3da02bcdcf8603
Chandu2411 <poornachandhu022@gmail.com>
2026-09-19 15:46:41 +0530

    Initial SignIn OAuth application

---
+++ src/test/java/com/core_spring/signin/SigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core_spring.signin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigninApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
